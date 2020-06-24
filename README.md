448d28f7f5b2bd0cee2bca45f647b710f35611ec
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-06-24 21:08:14 +0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Github-users-search-with-React-Hooks
-Github users search using Github API and React Hooks. I've used useReducer hook to substitute Redux.
-
-* You can search users
-* You can open any user's profile on a separate page
-* You can check all repos of a user
-
-### Посмотреть на опубликованную страничку можно [тут](https://gennady-bars.github.io/Github-users-search-with-React-Hooks-deployed-SPA/)
---
+++ precache-manifest.03e7241041173f5099ebe2eefa54e033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7088233aae7dae21064e7952d595696",
+    "url": "/Github-users-search-with-React-Hooks/index.html"
+  },
+  {
+    "revision": "39eef5ba05d46abaea02",
+    "url": "/Github-users-search-with-React-Hooks/static/css/main.69a9c519.chunk.css"
+  },
+  {
+    "revision": "f573e12758bf4e5b9ecb",
+    "url": "/Github-users-search-with-React-Hooks/static/js/2.3b8d8c88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-users-search-with-React-Hooks/static/js/2.3b8d8c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39eef5ba05d46abaea02",
+    "url": "/Github-users-search-with-React-Hooks/static/js/main.a692008b.chunk.js"
+  },
+  {
+    "revision": "baf7aba4a6ee6f6e84f6",
+    "url": "/Github-users-search-with-React-Hooks/static/js/runtime-main.395308e9.js"
+  },
+  {
+    "revision": "23b8ca23938cdddef47d5c1a63efccc5",
+    "url": "/Github-users-search-with-React-Hooks/static/media/git-disco.23b8ca23.gif"
+  }
+]);
